78f41104041562ae4fe816d44aa263bc0bc82f67
jamesliauw <jamesliauw@163.com>
2016-01-25 14:08:05 +0800

    测试可更改版本
    adb shell input keyevent 82 调出RELOAD菜单

--- index.android.js
+++ index.android.js
@@ -16,7 +16,7 @@ class MyReact extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome James Liauw!
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.android.js
